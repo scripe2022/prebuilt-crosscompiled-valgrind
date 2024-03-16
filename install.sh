b0fcb3da991e5828303742d96e431b9acb39dff9
jyhptr <im@jyh.sb>
2024-03-16 01:01:33 -0700

    Add: prebuilt libs

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+apt update
+apt upgrade -y
+apt install -y build-essential gcc-arm-linux-gnueabihf binutils-arm-linux-gnueabi qemu qemu-user-static gcc g++ libc6-dbg-armhf-cross
+
+cp -r prebuilt/usr_local_bin /usr/local/bin/
+cp -r prebuilt/usr_local_include_valgrind /usr/local/include/valgrind/
+cp -r prebuilt/usr_local_libexec_valgrind /usr/local/libexec/valgrind/
+cp -r prebuilt/usr_local_lib_pkgconfig /usr/local/lib/pkgconfig/
+cp -r prebuilt/usr_local_lib_valgrind /usr/local/lib/valgrind/
+
+cp /usr/arm-linux-gnueabihf/lib/ld-linux-armhf.so.3 /lib/
+cp -r /usr/arm-linux-gnueabihf/lib/debug/ /usr/lib/debug
+ln -s /usr/arm-linux-gnueabihf/lib/libc.so.6 /lib/
+
+chmod +x /usr/local/libexec/valgrind/memcheck-arm-linux
